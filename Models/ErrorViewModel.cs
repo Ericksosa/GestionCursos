a499a0b7a916ae296d3eb8ec4355ee99c07120be
Ericksosa <erick_sosa@bhd.com.do>
2026-02-10 21:16:55 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestiondeCursos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
